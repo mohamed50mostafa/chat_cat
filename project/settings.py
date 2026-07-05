bc68b05c0176114f24eb8e7d7e8c2a136d62701b
mohamed50mostafa <mohamedmostafakheder@outlook.com>
2026-07-06 01:01:24 +0300

    Fix CORS allowed origins trailing slash crash

--- project/settings.py
+++ project/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 # 2. تقييد الـ CORS (حظر الوصول المطلق)
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://chat-51zccrfq3-mohameds-projects-78431806.vercel.app/",  # رابط تطبيق الواجهة الأمامية الخاص بك فقط
+    "https://chat-51zccrfq3-mohameds-projects-78431806.vercel.app",  # رابط تطبيق الواجهة الأمامية الخاص بك فقط
 ]
 CORS_ALLOW_CREDENTIALS = True
 
